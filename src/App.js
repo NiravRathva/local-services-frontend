86201ce2d38fc7a7de1045a442e284edc2cee473
Niravrathva <niravrathwa57@gmail.com>
2023-09-09 22:45:37 +0530

    added Hero component and Images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Component/Navbar";
+import Home from "./Pages/Home";
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
-        <Routes></Routes>
+        <Routes>
+        <Route path="/" element={<Home />}></Route>
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/Component/Hero.js
@@ -0,0 +1,19 @@
+import "./Hero.css";
+
+const Hero = () => {
+   
+  return (
+    <div className="p-5 text-center  rounded-3 heroDiv">
+    <div className="mask">
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <div className="text-center position-absolute bottom-0 start-50 translate-middle-x text-white">
+            <h1>Quality Home Service </h1>
+            <p>At your doorstep in an hour </p>
+          </div>
+        </div>
+      </div>
+  </div>
+  )
+}
+
+export default Hero
