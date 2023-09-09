95afec5228610b32309e6ba1d0207c2c51865d2e
Niravrathva <niravrathwa57@gmail.com>
2023-09-09 23:34:58 +0530

    Integrated service data into the Packages component

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,15 +1,25 @@
+import { Link } from "react-router-dom";
 const Card = ({ services }) => {
   return (
     <div className="my-4 col-12 col-md-6 col-lg-4 ">
-      <div className="card" style={{ width: "18rem" }}>
-        <img src={services.imgUrl} className="card-img-top img-fluid" alt="..." />
-        <div
-          className="card-body text-white"
-          style={{ backgroundColor: "rgb(13,110,253)" }}
-        >
-          <h5 className="card-title ">{services.name}</h5>
+      <Link
+        to={`/packages/${services._id}`}
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={services.imgUrl}
+            className="card-img-top img-fluid"
+            alt="..."
+          />
+          <div
+            className="card-body text-white"
+            style={{ backgroundColor: "rgb(13,110,253)" }}
+          >
+            <h5 className="card-title ">{services.name}</h5>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/Pages/Packages.js
+++ src/Pages/Packages.js
@@ -1,21 +1,32 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLocation } from "react-router-dom";
+import useFetch from "../Hooks/useFetch";
 const Packages = () => {
+  const location = useLocation(); // React Hook
+  const id = location.pathname.split("/")[2];
+  const { data, loading, error, reFetch } = useFetch(
+    `https://localservice.onrender.com/api/v1/packages/${id}`
+  );
+console.log(data)
+  let service = "";
+  if (data.length !== 0) {
+    service = data[0].serviceName;
+  }
   return (
     <div className="container">
       <div className="row h-75 align-items-center">
         <div className="col  m-5 text-start">
-          <h1 className="fs-2">service name</h1>
+          <h1 className="fs-2">{service.name}</h1>
           <h6 className="">
-            <FontAwesomeIcon icon={faStar} /> 4.5{" "}
+            <FontAwesomeIcon icon={faStar} /> 4.5 (4k Bookings)
           </h6>
-          <p>Desc</p>
         </div>
         <div className="col m-5">
           <img
             className="rounded-4 img-fluid"
-            src=""
-            alt=""
+            src={service.imgUrl}
+            alt={service.name}
             // style={{ width: "100%", height: "100%", backgroundSize: "cover" }}
           />
         </div>
