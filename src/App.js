38c26b5737b5333edd640eef21f389331947d9eb
Niravrathva <niravrathwa57@gmail.com>
2023-09-10 00:21:47 +0530

    added  Contact Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Component/Navbar";
 import Home from "./Pages/Home";
 import Packages from "./Pages/Packages";
 import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
         <Route path="packages/:id" element={<Packages />} />
         </Routes>
       </Router>
---
+++ src/Pages/Contact.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container mt-5 " style={{ height: "100vh" }}>
+      <div className="row  ">
+        <div
+          className="col-md-6 "
+          style={{
+            backgroundImage:
+              "url(https://img.freepik.com/free-vector/flat-customer-support-illustration_23-2148899114.jpg?size=626&ext=jpg&ga=GA1.2.423171406.1685598216&semt=robertav1_2_sidr)",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="">
+            <h2>Contact Information</h2>
+            <p>123 Main Street</p>
+            <p>City, State, ZIP</p>
+            <p>Phone: (123) 456-7890</p>
+            <p>Email: info@example.com</p>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h2>Contact Form</h2>
+          <form>
+            <div className="form-group my-2">
+              <label for="name">Name:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter your name"
+              />
+            </div>
+            <div className="form-group my-2">
+              <label for="email">Email:</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="form-group my-2">
+              <label for="message">Message:</label>
+              <textarea
+                className="form-control"
+                id="message"
+                rows="5"
+                placeholder="Enter your message"
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary my-2 w-100">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
