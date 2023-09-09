dbf1e74a9c45e6b222009b31a66d81578d8d5684
Niravrathva <niravrathwa57@gmail.com>
2023-09-10 00:07:02 +0530

    Added About Us page with a description and an image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Component/Navbar";
 import Home from "./Pages/Home";
 import Packages from "./Pages/Packages";
+import About from "./Pages/About";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Navbar/>
         <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
         <Route path="packages/:id" element={<Packages />} />
         </Routes>
       </Router>
---
+++ src/Pages/About.js
@@ -0,0 +1,42 @@
+const About = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            eleifend risus ac lobortis pellentesque. Sed interdum, lacus nec
+            viverra cursus, nulla mauris feugiat tortor, id vulputate lectus
+            odio ut mauris. Duis pretium tincidunt consequat. Vestibulum ante
+            ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+            Curae; Nullam non turpis erat. Proin vel enim sed odio aliquam
+            interdum. Quisque dapibus interdum sapien, a interdum elit suscipit
+            a. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus
+            orci luctus et ultrices posuere cubilia Curae; Nam at luctus leo.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            eleifend risus ac lobortis pellentesque. Sed interdum, lacus nec
+            viverra cursus, nulla mauris feugiat tortor, id vulputate lectus
+            odio ut mauris. Duis pretium tincidunt consequat. Vestibulum ante
+            ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+            Curae; Nullam non turpis erat. Proin vel enim sed odio aliquam
+            interdum. Quisque dapibus interdum sapien, a interdum elit suscipit
+            a. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus
+            orci luctus et ultrices posuere cubilia Curae; Nam at luctus leo.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <img
+            src="https://img.freepik.com/free-vector/teamwork-concept-landing-page_52683-21301.jpg?size=626&ext=jpg"
+            alt="About Us"
+            className="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
