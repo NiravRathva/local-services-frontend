3dfd366e2b1f9db1fc30b4c9d3e6280f78bdda52
Niravrathva <niravrathwa57@gmail.com>
2023-09-11 18:24:32 +0530

    refactored styling of register page

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -8,8 +8,6 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  
-
   const navigate = useNavigate();
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -46,41 +44,59 @@ const Register = () => {
   };
 
   return (
-    <div className="bg-light d-flex align-item-center justify-content-center " style={{height:"100vh"}}>
-      <form className="lContainer  position-absolute start-50 top-50 translate-middle  border border-dark p-5">
-      <h3>Create Account</h3>
-        {/* Input fields with onChange handlers */}
-        <input className="form-control"
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-        />
-        <input className="form-control"
-          type="text"
-          name="mobileNo"
-          value={mobileNo}
-          onChange={(e) => setMobileNo(e.target.value)}
-          placeholder="Mobile Number"
-        />
-        <input className="form-control"
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input className="form-control"
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-
-        {/* Submit button */}
-        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+    <div
+      className="bg-light d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <form className="container p-5 border border-dark rounded">
+        <h3 className="mb-4">Create Account</h3>
+        <div className="mb-3">
+          {/* Input fields with onChange handlers */}
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="mobileNo"
+            value={mobileNo}
+            onChange={(e) => setMobileNo(e.target.value)}
+            placeholder="Mobile Number"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+        {/* Submit button */}{" "}
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </form>
